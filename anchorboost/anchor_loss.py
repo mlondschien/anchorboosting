95c7fb798dd9b6f4a4c20f6fef0bf5fc8a21631d
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2022-09-09 13:48:43 +0200

    Anchor loss (#2)
    * Use negative-log-likelhiood.
    
    * Some docstrings and tests.
    
    * Speed up grad tests.

--- anchorboost/anchor_loss.py
+++ anchorboost/anchor_loss.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import logsumexp
 
 
 class AnchorMixin:
@@ -11,29 +12,61 @@ class AnchorMixin:
 
     def loss(self, f, y, anchor):
         residuals = self.residuals(f, y)
+
+        if self.gamma == 1:
+            return residuals ** 2
+
         return residuals ** 2 + (self.gamma - 1) * self._proj(anchor, residuals) ** 2
 
-    def anchor_objective(self, f, data):
+    def objective(self, f, data):
+        """Objective function for LGBM."""
         y = data.get_label()
         anchor = data.anchor
         return self.grad(f, y, anchor), self.hess(f, y, anchor)
 
-    def anchor_score(self, f, data):
+    def score(self, f, data):
+        """Score function for LGBM."""
         y = data.get_label()
         anchor = data.anchor
-        return self.name, self.loss(f, y, anchor).mean(), self.higher_is_better
+        return (
+            f"{self.name}_{self.gamma}",
+            self.loss(f, y, anchor).mean(),
+            self.higher_is_better,
+        )
 
     def _proj(self, anchor, f):
+        """Project f onto the subspace spanned by anchor.
+
+        Parameters
+        ----------
+        anchor: np.ndarray of dimension (n, d_anchor).
+            The anchor matrix. If d_anchor = 1 and entries are integer, assumed to be
+            categories.
+        f: np.ndarray of dimension (n, d_f).
+            The vector to project.
+
+        Returns
+        -------
+        np.ndarray of dimension (n, d_f).
+            Projection of f onto the subspace spanned by anchor.
+        """
         # Linear projection of f onto the column space of anchor.
+        if anchor.shape[1] == 1 and "int" in str(anchor.dtype):
+            projected_values = np.zeros(f.shape)
+            for unique_value in np.unique(anchor):
+                mask = anchor.flatten() == unique_value
+                projected_values[mask, :] = f[mask, :].mean(axis=0)
+            return projected_values
+
         return np.dot(anchor, np.linalg.lstsq(anchor, f, rcond=None)[0])
-        # return np.dot(self._proj_matrix(anchor), f)
 
     def _proj_matrix(self, a):
         assert a.shape[1] < a.shape[0]
         return np.dot(np.dot(a, np.linalg.inv(a.T @ a)), a.T)
 
     def hess(self, f, y, anchor):
-        return 2 * np.ones(len(y))
+        """Trivial hessian."""
+        return 2 * np.ones(f.size)
 
 
 class AnchorRegressionLoss(AnchorMixin):
@@ -69,11 +102,27 @@ class AnchorClassificationLoss(AnchorMixin):
         assert (sorted(unique_values) == unique_values).all()
         return np.array([counts / sum(unique_counts) for counts in unique_counts])
 
+    def objective(self, f, data):
+        y = data.get_label()
+        f = np.reshape(f, (len(y), -1))
+        anchor = data.anchor
+        return self.grad(f, y, anchor).flatten("C"), self.hess(f, y, anchor)
+
+    def score(self, f, data):
+        y = data.get_label()
+        f = np.reshape(f, (len(y), -1))
+        anchor = data.anchor
+        return self.name, self.loss(f, y, anchor).mean(), True
+
     def loss(self, f, y, anchor):
+        if self.gamma == 1:
+            return self.negative_log_likelihood(f, y)
+
         residuals = self.residuals(f, y)
-        return self.likelihood(f, y) + (self.gamma - 1) * np.sum(
+        loss = self.negative_log_likelihood(f, y) + (self.gamma - 1) * np.sum(
             self._proj(anchor, residuals) ** 2, axis=1
         )
+        return loss
 
     def predictions(self, f):
         f = f - np.max(f)  # normalize f to avoid overflow
@@ -91,7 +140,7 @@ class AnchorClassificationLoss(AnchorMixin):
         return residuals
 
     def grad(self, f, y, anchor):
-        f = f - np.max(f)  # normalize f to avoid overflow
+        f = f - np.max(f, axis=1)[:, np.newaxis]  # normalize f to avoid overflow
         divisor = np.sum(np.exp(f), axis=1)
         predictions = np.exp(f) / divisor[:, np.newaxis]
         residuals = self.residuals(f, y)
@@ -100,13 +149,13 @@ class AnchorClassificationLoss(AnchorMixin):
             -np.sum(predictions * projected_residuals, axis=1)[:, np.newaxis]
             + projected_residuals
         )
-        return -2 * residuals + 2 * (self.gamma - 1) * grad
+        return +2 * residuals + 2 * (self.gamma - 1) * grad
 
     def _indices(self, y, n_classes):
-        return (np.arange(len(y)), y)
+        return (np.arange(len(y)), y.astype(int))
 
-    def likelihood(self, f, y):
+    def negative_log_likelihood(self, f, y):
         f = f - np.max(f)
-        divisor = np.sum(np.exp(f), axis=1)
         indices = self._indices(y, f.shape[1])
-        return np.exp(f[indices]) / divisor
+        log_divisor = logsumexp(f, axis=1)[:, np.newaxis]
+        return -f[indices] + log_divisor
--- tests/test_anchor_loss.py
+++ tests/test_anchor_loss.py
@@ -5,28 +5,28 @@ from anchorboost.anchor_loss import AnchorClassificationLoss, AnchorRegressionLo
 from anchorboost.simulate import f2, simulate
 
 
-@pytest.mark.parametrize("gamma", [0.1, 1, 10, 100, 1000])
+@pytest.mark.parametrize("gamma", [0.1, 1, 2, 10, 100, 1000])
 def test_regression_grad(gamma):
     loss = AnchorRegressionLoss(gamma)
     _, y, a = simulate(f2)
     rng = np.random.RandomState(0)
     f = rng.normal(size=len(y))
-    check_gradient(loss.loss, loss.grad, f, y, a, stepsize=0.1 / (1 + gamma))
+    check_gradient(loss.loss, loss.grad, f, y, a, stepsize=1 / (1 + gamma))
 
 
-@pytest.mark.parametrize("gamma", [0.1, 1, 10, 100, 1000])
+@pytest.mark.parametrize("gamma", [0.1, 1, 2, 10, 100, 1000])
 def test_classification_grad(gamma):
     loss = AnchorClassificationLoss(gamma)
     _, y, a = simulate(f2)
     y = (y >= 0).astype(np.int64) + (y >= 1).astype(np.int64)
     rng = np.random.RandomState(0)
     f = rng.normal(size=(len(y), len(loss.init_score(y))))
-    check_gradient(loss.loss, loss.grad, f, y, a, stepsize=0.1 / (1 + gamma))
+    check_gradient(loss.loss, loss.grad, f, y, a, stepsize=1 / (1 + gamma))
 
 
 def check_gradient(loss, grad, f, y, anchor, stepsize):
     before = loss(f, y, anchor).sum()
-    for i in range(1000):
+    for i in range(200):
         f = f - stepsize * grad(f, y, anchor)
         after = loss(f, y, anchor).sum()
         if not after - before <= 1e-14:
@@ -39,7 +39,7 @@ def check_gradient(loss, grad, f, y, anchor, stepsize):
 def test_indices():
     loss = AnchorClassificationLoss(1)
     y = np.array([1, 3, 2, 2])
-    indices = loss._indices(y, 5)
+    indices = loss._indices(y)
 
     array = np.zeros((4, 5))
     for i in range(4):
@@ -48,10 +48,32 @@ def test_indices():
     np.testing.assert_equal(array[indices], np.arange(4))
 
 
-def test_proj():
+@pytest.mark.parametrize(
+    "anchor, residuals, result",
+    [
+        (
+            np.array([[0.0, 1.0], [0.0, 1.0], [1.0, 0]]),
+            np.array([[1], [2], [3]]),
+            np.array([[1.5], [1.5], [3]]),
+        ),
+        (
+            np.array([[2], [2], [1], [3]]),
+            np.array([[1], [2], [3], [1]]),
+            np.array([[1.5], [1.5], [3], [1]]),
+        ),
+        (
+            np.array([[1.0], [1.0], [0.0]]),
+            np.array([[1, 2], [0, 2], [3, 4]]),
+            np.array([[0.5, 2.0], [0.5, 2.0], [0.0, 0.0]]),
+        ),
+        (
+            np.array([[2], [2], [1]]),
+            np.array([[1, 2], [0, 2], [3, 4]]),
+            np.array([[0.5, 2.0], [0.5, 2.0], [3.0, 4.0]]),
+        ),
+        (np.array([[0.0]]), np.array([[1]]), np.array([[0]])),
+    ],
+)
+def test_proj(anchor, residuals, result):
     loss = AnchorRegressionLoss(1)
-    _, y, a = simulate(f2)
-    residuals = y - loss.init_score(y)
-    np.testing.assert_almost_equal(
-        loss._proj_matrix(a) @ residuals, loss._proj(a, residuals)
-    )
+    np.testing.assert_almost_equal(loss._proj(anchor, residuals), result)
