1d56cc7b78b55e1ab3b9033da9f2d67bb80fb953
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2023-05-03 10:02:58 +0200

    Speed up projection for discrete anchors (#15)
    * Speed up projection for discrete anchors.
    
    * Revert setuptools.

--- tests/test_anchor_mixin.py
+++ tests/test_anchor_mixin.py
@@ -6,42 +6,59 @@ from anchorboost import AnchorMixin
 
 cases = [
     (
-        np.array([[0.0, 1.0], [0.0, 1.0], [1.0, 0]]),
+        np.array([[0], [0], [1]]),
         np.array([[1], [2], [3]]),
         np.array([[1.5], [1.5], [3]]),
     ),
-    # (
-    #     np.array([[2], [2], [1], [3]]),
-    #     np.array([[1], [2], [3], [1]]),
-    #     np.array([[1.5], [1.5], [3], [1]]),
-    # ),
     (
-        np.array([[1.0], [1.0], [0.0]]),
-        np.array([[1, 2], [0, 2], [3, 4]]),
-        np.array([[0.5, 2.0], [0.5, 2.0], [0.0, 0.0]]),
+        np.array([[2], [2], [1], [3]]),
+        np.array([[1], [2], [3], [1]]),
+        np.array([[1.5], [1.5], [3], [1]]),
     ),
     (
-        np.array([[1.0], [1.0], [0.0]]),
+        np.array([[1], [1], [0]]),
+        np.array([[1, 2], [0, 2], [3, 4]]),
+        np.array([[0.5, 2.0], [0.5, 2.0], [3.0, 4.0]]),
+    ),
+    (
+        np.array([[1], [1], [0]]),
         np.array([1, 0, 3]),
-        np.array([0.5, 0.5, 0.0]),
+        np.array([0.5, 0.5, 3]),
     ),
     (
-        np.array([[1.0], [1.0], [0.0]]),
+        np.array([[1], [1], [0]]),
         np.array([[1, 2], [0, 2], [3, 4]]),
-        np.array([[0.5, 2.0], [0.5, 2.0], [0.0, 0.0]]),
+        np.array([[0.5, 2.0], [0.5, 2.0], [3.0, 4.0]]),
     ),
-    # (
-    #     np.array([[2], [2], [1]]),
-    #     np.array([[1, 2], [0, 2], [3, 4]]),
-    #     np.array([[0.5, 2.0], [0.5, 2.0], [3.0, 4.0]]),
-    # ),
-    # (np.array([[0.0]]), np.array([[1]]), np.array([[0]])),
+    (
+        np.array([[2], [2], [1]]),
+        np.array([[1, 2], [0, 2], [3, 4]]),
+        np.array([[0.5, 2.0], [0.5, 2.0], [3.0, 4.0]]),
+    ),
+    (
+        np.array([[0.0, 2.0], [0.0, 2.0], [1.0, 0.0], [0.0, 0.0]]),
+        np.array([[1.0], [2.0], [3.0], [4.0]]),
+        np.array([[1.5], [1.5], [3.0], [4.0]]),
+    ),
+    (np.array([[0.0]]), np.array([[1]]), np.array([[1]])),
 ]
 
 
+def _one_hot(arr):
+    unique, index = np.unique(arr, return_inverse=True)
+    return np.eye(len(unique))[index]
+
+
 @pytest.mark.parametrize("anchor, residuals, result", cases)
-def test_proj_result(anchor, residuals, result):
+@pytest.mark.parametrize("one_hot", [True, False])
+def test_proj_result(one_hot, anchor, residuals, result):
     loss = AnchorMixin()
+
+    if one_hot:
+        if anchor.shape[1] != 1:
+            pytest.skip("one_hot only makes sense for 1D anchors")
+        anchor = _one_hot(anchor)
+
     np.testing.assert_almost_equal(loss._proj(anchor, residuals), result)
 
 
--- tests/test_compare_to_lgbm.py
+++ tests/test_compare_to_lgbm.py
@@ -111,7 +111,20 @@ def test_multi_classification_to_lgbm():
     np.testing.assert_allclose(lgb_pred, my_pred, rtol=1e-5)
 
 
-def test_regression_to_lgbm():
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        {},
+        # {"colsample_bytree": 0.6},
+        {"max_depth": -1},
+        {"num_leaves": 63},
+        {"min_split_gain": 0.002},
+        {"reg_lambda": 2},
+        {"subsample_freq": 0},
+        {"subsample": 0.2},
+    ],
+)
+def test_regression_to_lgbm(parameters):
     class RegressionObjective(LGBMMixin, RegressionMixin):
         pass
 
@@ -121,13 +134,13 @@ def test_regression_to_lgbm():
     data = lgb.Dataset(X, y, init_score=loss.init_score(y))
 
     lgb_model = lgb.train(
-        params={"learning_rate": 0.1, "objective": "regression"},
+        params={"learning_rate": 0.1, "objective": "regression", **parameters},
         train_set=data,
         num_boost_round=10,
     )
 
     my_model = lgb.train(
-        params={"learning_rate": 0.1},
+        params={"learning_rate": 0.1, **parameters},
         train_set=data,
         num_boost_round=10,
         fobj=loss.objective,
