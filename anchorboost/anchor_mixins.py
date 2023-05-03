b0db46eb9da1c10a7ab9e20c6c6de5dedfd9ec29
Malte Londschien <malte@londschien.de>
2023-05-03 09:48:29 +0200

    Speed up projection for discrete anchors.

--- anchorboost/anchor_mixins.py
+++ anchorboost/anchor_mixins.py
@@ -18,7 +18,20 @@ class AnchorMixin:
         np.ndarray of dimension (n, d_f) or (n,)
             Projection of f onto the subspace spanned by anchor. Same dimension as f.
         """
-        return np.dot(anchor, np.linalg.lstsq(anchor, f, rcond=None)[0])
+        if anchor.shape[1] == 1 and anchor.dtype == int:
+            out = np.zeros(f.shape)
+            for unique_value in np.unique(anchor):
+                mask = (anchor == unique_value).flatten()
+                if len(f.shape) == 1:
+                    out[mask] = f[mask].mean()
+                else:
+                    out[mask, :] = f[mask, :].mean(axis=0)
+            return out
+
+        anchor = anchor - anchor.mean(axis=0)
+        f_mean = f.mean(axis=0)
+
+        return np.dot(anchor, np.linalg.lstsq(anchor, f, rcond=None)[0]) + f_mean
 
     def _proj_matrix(self, a):
         """Projection matrix onto the subspace spanned by a."""
