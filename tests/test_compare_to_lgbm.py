5366258ce8730ee9bb6a1f6016b854a163d15224
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2025-04-21 14:00:56 +0200

    Test refit (#21)
    * That's what tests are for.
    
    * strings are allowed as well.
    
    * Allow Z of shape (n,)
    
    * Overwrite in proj
    
    * Categories.
    
    * Don't use k-class formulation
    
    * Fix utils test.
    
    * Add refit test.
    
    * lint
    
    * Bump pre-commit action.
    
    * Bump pre-commit action.

--- tests/test_compare_to_lgbm.py
+++ tests/test_compare_to_lgbm.py
@@ -39,13 +39,12 @@ def test_classification_to_lgbm(parameters):
     y = (y > 0).astype(int)
 
     loss1 = ClassificationObjective()
-    loss2 = MultiClassificationObjective(2)
-    loss3 = AnchorKookClassificationObjective(gamma=1)
+    loss2 = AnchorKookClassificationObjective(gamma=1)
 
+    data0 = lgb.Dataset(X, y)
     data1 = lgb.Dataset(X, y, init_score=loss1.init_score(y))
     data2 = lgb.Dataset(X, y, init_score=loss2.init_score(y))
-    data3 = lgb.Dataset(X, y, init_score=loss3.init_score(y))
-    data3.anchor = a
+    data2.anchor = a
 
     params = {
         "random_state": 0,
@@ -55,7 +54,7 @@ def test_classification_to_lgbm(parameters):
     }
     model0 = lgb.train(
         params={**params, "objective": "binary", **parameters},
-        train_set=data1,
+        train_set=data0,
         num_boost_round=10,
     )
     model1 = lgb.train(
@@ -65,33 +64,21 @@ def test_classification_to_lgbm(parameters):
     )
 
     model2 = lgb.train(
-        params={**params, "objective": loss2.objective, "num_class": 2, **parameters},
+        params={**params, "objective": loss2.objective, **parameters},
         train_set=data2,
         num_boost_round=10,
     )
 
-    model3 = lgb.train(
-        params={**params, "objective": loss3.objective, **parameters},
-        train_set=data3,
-        num_boost_round=10,
-    )
-
     pred0 = model0.predict(X)
-    pred1 = loss1.predictions(model1.predict(X))
-    pred2 = loss2.predictions(model2.predict(X))[:, 1]
-    pred3 = loss3.predictions(model3.predict(X))
+    pred1 = loss1.predictions(model1.predict(X) + loss1.init_score(y))
+    pred2 = loss2.predictions(model2.predict(X) + loss2.init_score(y))
 
     np.testing.assert_allclose(pred0, pred1, rtol=1e-5)
-
-    np.testing.assert_allclose(pred0, pred3, rtol=1e-5)
-
-    if "reg_lambda" not in parameters:
-        np.testing.assert_allclose(pred0, pred2, rtol=1e-5)
+    np.testing.assert_allclose(pred0, pred2, rtol=1e-5)
 
 
 def test_multi_classification_to_lgbm():
-
-    X, y, a = simulate(f1, shift=0, seed=0)
+    X, y, _ = simulate(f1, shift=0, seed=0)
     y = (y > 0).astype(int) + (y > 1).astype(int)
 
     loss = MultiClassificationObjective(n_classes=3)
@@ -119,7 +106,7 @@ def test_multi_classification_to_lgbm():
     "parameters",
     [
         {},
-        # {"colsample_bytree": 0.6},
+        # {"colsample_bytree": 0.6}, https://github.com/microsoft/LightGBM/issues/5543
         {"max_depth": -1},
         {"num_leaves": 63},
         {"min_split_gain": 0.002},
@@ -137,12 +124,14 @@ def test_regression_to_lgbm(parameters):
     loss1 = RegressionObjective()
     loss2 = AnchorRegressionObjective(gamma=1)
 
+    data0 = lgb.Dataset(X, y, init_score=np.ones_like(y) * np.mean(y))
     data1 = lgb.Dataset(X, y, init_score=loss1.init_score(y))
     data2 = lgb.Dataset(X, y, init_score=loss2.init_score(y))
+    data2.anchor = a
 
     model0 = lgb.train(
         params={"learning_rate": 0.1, "objective": "regression", **parameters},
-        train_set=data1,
+        train_set=data0,
         num_boost_round=10,
     )
 
@@ -158,9 +147,89 @@ def test_regression_to_lgbm(parameters):
         num_boost_round=10,
     )
 
-    pred0 = model0.predict(X)
-    pred1 = model1.predict(X)
-    pred2 = model2.predict(X)
+    pred0 = model0.predict(X) + y.mean()
+    pred1 = model1.predict(X) + loss1.init_score(y)
+    pred2 = model2.predict(X) + loss2.init_score(y)
 
+    np.testing.assert_allclose(pred0, pred1, rtol=1e-6)
+    np.testing.assert_allclose(pred0, pred2, rtol=1e-6)
+
+
+@pytest.mark.parametrize(
+    "decay_rate",
+    [0.0, 0.5, 1.0],
+)
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        {},
+        # {"colsample_bytree": 0.6}, https://github.com/microsoft/LightGBM/issues/5543
+        {"max_depth": -1},
+        {"num_leaves": 63},
+        {"min_split_gain": 0.002},
+        {"reg_lambda": 0.1},
+        {"subsample_freq": 0},
+        {"subsample": 0.2},
+    ],
+)
+def test_compare_refit_to_lgbm(parameters, decay_rate):
+    class RegressionObjective(LGBMMixin, RegressionMixin):
+        pass
+
+    X, y, a = simulate(f1, shift=0, seed=0)
+
+    loss1 = RegressionObjective()
+    loss2 = AnchorRegressionObjective(gamma=1)
+
+    data0 = lgb.Dataset(X, y, init_score=loss1.init_score(y))
+    data1 = lgb.Dataset(X, y, init_score=loss2.init_score(y))
+    data2 = lgb.Dataset(X, y, init_score=loss2.init_score(y))
+    data2.anchor = a
+
+    model0 = lgb.train(
+        params={"learning_rate": 0.1, "objective": "regression", **parameters},
+        train_set=data0,
+        num_boost_round=10,
+    )
+
+    model1 = lgb.train(
+        params={"learning_rate": 0.1, "objective": loss1.objective, **parameters},
+        train_set=data1,
+        num_boost_round=10,
+    )
+
+    model2 = lgb.train(
+        params={"learning_rate": 0.1, "objective": loss2.objective, **parameters},
+        train_set=data2,
+        num_boost_round=10,
+    )
+
+    X_new, y_new, _ = simulate(f1, shift=1, seed=0)
+    model0 = model0.refit(
+        data=X_new,
+        label=y_new,
+        decay_rate=decay_rate,
+        init_score=loss1.init_score(y_new),
+    )
+    model1._Booster__set_objective_to_none = False
+    model1.params["objective"] = "regression"
+    model1 = model1.refit(
+        data=X_new,
+        label=y_new,
+        decay_rate=decay_rate,
+        init_score=loss1.init_score(y_new),
+    )
+    model2._Booster__set_objective_to_none = False
+    model2.params["objective"] = "regression"
+    model2 = model2.refit(
+        data=X_new,
+        label=y_new,
+        decay_rate=decay_rate,
+        init_score=loss2.init_score(y_new),
+    )
+
+    pred0 = model0.predict(X_new)
+    pred1 = model1.predict(X_new)
+    pred2 = model2.predict(X_new)
     np.testing.assert_allclose(pred0, pred1, rtol=1e-5)
     np.testing.assert_allclose(pred0, pred2, rtol=1e-5)
