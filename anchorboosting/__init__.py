28db05825e4c604aa75748e016682b504c5aed04
Malte Londschien <malte@londschien.de>
2025-04-23 18:05:41 +0200

    Format.

--- anchorboosting/__init__.py
+++ anchorboosting/__init__.py
@@ -1,6 +1,4 @@
-from .objectives.anchor_objectives import AnchorRegressionObjective
 from .models import AnchorBooster
+from .objectives.anchor_objectives import AnchorRegressionObjective
 
-__all__ = [
-    "AnchorRegressionObjective", "AnchorBooster"
-]
+__all__ = ["AnchorRegressionObjective", "AnchorBooster"]
