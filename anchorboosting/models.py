bdd0210f4ac0d16e3cfafef5a9c5290faaf36931
Malte Londschien <malte@londschien.de>
2025-05-14 17:05:40 +0200

    Make probit anchor regression more stable.

--- anchorboosting/models.py
+++ anchorboosting/models.py
@@ -158,16 +158,17 @@ class AnchorBooster:
             # loss(f, y) = - sum_i (y_i log(p_i) + (1 - y_i) log(1 - p_i))
             # where p_i = P(f > 0) (Gaussian cdf)
             elif self.objective == "probit":
-                p = scipy.stats.norm.cdf(f)
-                dp = scipy.stats.norm.pdf(f)  # d/df p(f)
-                A = np.where(y == 1, -1 / p, 1 / (1 - p))
-                r = A * dp  # d/df loss(f, y)
-                dr = -f * dp * A + dp**2 * A**2  # d^2/df^2 loss(f, y)
-                ddr = (
-                    (f**2 - 1) * dp * A
-                    - 3 * f * dp**2 * A**2
-                    + 2 * dp**3 * A**3
-                )
+                # We wish to compute the following:
+                # p = scipy.stats.norm.cdf(f)
+                # dp = scipy.stats.norm.pdf(f)  # d/df p(f)
+                # r = np.where(y == 1, -dp / p, dp / (1 - p))  # d/df loss(f, y)
+                # This is numerically unstable. Instead, we use scipy.special.log_ndtr
+                # with log_ndtr(f) = log(norm.cdf(f)).
+                y_tilde = np.where(y == 1, 1, -1)
+                log_phi = -0.5 * f**2 - 0.5 * np.log(2 * np.pi)  # log(norm.pdf(f))
+                r = -y_tilde * np.exp(log_phi - scipy.special.log_ndtr(y_tilde * f))
+                dr = -f * r + r**2  # d^2/df^2 loss(f, y)
+                ddr = (f**2 - 1) * r - 3 * f * r**2 + 2 * r**3
             else:
                 raise ValueError(
                     "Objective must be one of 'regression', 'logistic', or 'probit'. "
