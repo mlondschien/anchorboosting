b0c2ed48e17e6ed8d3604f855c915328eb6343bb
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2025-05-14 12:42:39 +0300

    Classification boosting (#28)
    * Anchor classification.
    
    * Add * 2 in anchor classification hessian.
    
    * Make honest splits optional.
    
    * No more honest splits.
    
    * Fix docs
    
    * Transform to numpy array
    
    * Revert "Transform to numpy array"
    
    This reverts commit fd852b3f742fb05c98a6e57db65147c896aaf039.
    
    * pass different hessian for min_hessian_in_leaf
    
    * pass different hessian for min_hessian_in_leaf
    
    * Smaller max gamma
    
    * Add test_derivatives test
    
    * new test.
    
    * Use scipy.special.expit
    
    * Add lambda_l2
    
    * Probit regression
    
    * Format
    
    * Bring honest splits back
    
    * Test and better error
    
    * Typos and bug fixes.
    
    * Add failing test
    
    * Fix tests.
    
    * Format test_derivatives.py

--- anchorboosting/models.py
+++ anchorboosting/models.py
@@ -23,9 +23,18 @@ class AnchorBooster:
         The parameters for the LightGBM dataset. See LightGBM documentation for details.
     num_boost_round: int
         The number of boosting iterations. Default is 100.
-    honest_split_ratio: float, optional, default=0.5
-        Ratio of training data used to determine tree splits. The rest is used to
-        determine leaf values.
+    objective: str, optional, default="regression"
+        The objective function to use. Can be "regression", "logistic", or "probit".
+    honest_splits: bool, optional, default=False
+        If True, uses a different part of the training data to compute tree splits and
+        leaf values.
+    honest_splits_ratio: float, optional, default=0.5
+        Proportion of the training data to use to compute tree splits when honest_splits
+        is True. Must be in (0, 1).
+    subsample: float, optional, default=1.0
+        The fraction of samples to use for each boosting iteration. Must be in (0, 1].
+        Acts multiplicatively with ``honest_splits_ratio``. Not supported when
+        ``honest_splits`` is False.
     **kwargs: dict
         Additional parameters for the LightGBM model. See LightGBM documentation for
         details.
@@ -36,7 +45,10 @@ class AnchorBooster:
         gamma,
         dataset_params=None,
         num_boost_round=100,
-        honest_split_ratio=0.5,
+        objective="regression",
+        honest_splits=False,
+        honest_splits_ratio=0.5,
+        subsample=1.0,
         **kwargs,
     ):
         self.gamma = gamma
@@ -45,7 +57,10 @@ class AnchorBooster:
         self.num_boost_round = num_boost_round
         self.booster = None
         self.init_score_ = None
-        self.honest_split_ratio = honest_split_ratio
+        self.objective = objective
+        self.honest_splits = honest_splits
+        self.honest_splits_ratio = honest_splits_ratio
+        self.subsample = subsample
 
     def fit(
         self,
@@ -76,7 +91,24 @@ class AnchorBooster:
             feature_name = None
 
         dtype = np.result_type(Z, y)
-        self.init_score_ = np.mean(y)
+
+        if self.objective == "regression":
+            self.init_score_ = np.mean(y)
+        elif self.objective == "probit":
+            self.init_score_ = scipy.stats.norm.ppf(np.mean(y))
+        else:
+            self.init_score_ = np.log(np.mean(y) / (1 - np.mean(y)))
+
+        if self.objective != "regression" and not np.isin(y, [0, 1]).all():
+            raise ValueError("For binary classification, y values must be in {0, 1}.")
+
+        if self.subsample != 1 and not self.honest_splits:
+            raise ValueError(
+                "Subsampling is not supported when honest_splits is False. "
+                "Set honest_splits=True to use subsampling."
+            )
+
+        y = y.flatten()
 
         dataset_params = {
             "data": X,
@@ -90,19 +122,77 @@ class AnchorBooster:
         data = lgb.Dataset(**dataset_params)
 
         self.booster = lgb.Booster(params=self.params, train_set=data)
-        residuals = y - dataset_params["init_score"]
+        f = dataset_params["init_score"]  # scores
 
         Q, _ = np.linalg.qr(Z, mode="reduced")  # P_Z f = Q @ (Q^T @ f)
 
-        rng = np.random.default_rng(self.params.get("random_state", 0))
-        mask = np.empty_like(residuals, dtype=np.bool_)
-        mask[: int(len(residuals) * self.honest_split_ratio)] = True
-        mask[int(len(residuals) * self.honest_split_ratio) :] = False
+        if self.honest_splits:
+            n_split = int(len(y) * self.honest_splits_ratio * self.subsample)
 
-        grad = np.empty(len(y), dtype=dtype)
+            split_mask = np.zeros(len(y), dtype=bool)
+            split_mask[:n_split] = True
+            leaf_mask = np.zeros(len(y), dtype=bool)
+            leaf_mask[n_split : int(len(y) * self.subsample)] = True
+            rng = np.random.default_rng(self.params.get("random_state", 0))
 
         for idx in range(self.num_boost_round):
-            rng.shuffle(mask)
+            if self.honest_splits:
+                perm = rng.permutation(len(y))
+                split_mask = split_mask[perm]
+                leaf_mask = leaf_mask[perm]
+
+            # For regression, the loss (without anchor) is
+            # loss(f, y) = 0.5 * || y - f ||^2
+            if self.objective == "regression":
+                r = f - y  # d/df loss(f, y)
+                dr = np.ones(len(y), dtype=dtype)  # d^2/df^2 loss(f, y)
+                ddr = np.zeros(len(y), dtype=dtype)  # d^3/df^3 loss(f, y)
+            # For logistic regression, the loss (without anchor) is
+            # loss(f, y) = - sum_i (y_i log(p_i) + (1 - y_i) log(1 - p_i))
+            elif self.objective == "logistic":
+                p = scipy.special.expit(f)
+                r = p - y  # score residuals: r(f, y) = d/df loss(f, y)
+                dr = p * (1 - p)  # d/df r(f, y) = d^2/df^2 loss(f, y)
+                ddr = p * (1 - p) * (1 - 2 * p)  # d^3/df^3 loss(f, y)
+            # For probit regression, the loss (without anchor) is
+            # loss(f, y) = - sum_i (y_i log(p_i) + (1 - y_i) log(1 - p_i))
+            # where p_i = P(f > 0) (Gaussian cdf)
+            elif self.objective == "probit":
+                p = scipy.stats.norm.cdf(f)
+                dp = scipy.stats.norm.pdf(f)  # d/df p(f)
+                A = np.where(y == 1, -1 / p, 1 / (1 - p))
+                r = A * dp  # d/df loss(f, y)
+                dr = -f * dp * A + dp**2 * A**2  # d^2/df^2 loss(f, y)
+                ddr = (
+                    (f**2 - 1) * dp * A
+                    - 3 * f * dp**2 * A**2
+                    + 2 * dp**3 * A**3
+                )
+            else:
+                raise ValueError(
+                    "Objective must be one of 'regression', 'logistic', or 'probit'. "
+                    f" Got {self.objective}."
+                )
+
+            if self.honest_splits:
+                Q_ = Q[split_mask, :]
+                r_ = r[split_mask]
+                dr_ = dr[split_mask]
+                # Q_ @ (Q_.T @ r_) is Z[mask, :] @ (Z.T @ Z) @ (Z[mask, :]^T @ r_)
+                # That is, we use the full Z matrix to compute its covariance. However,
+                # different number of samples are used, so we need to rescale.
+                r_proj = Q_ @ (Q_.T @ r_) / (self.honest_splits_ratio * self.subsample)
+
+                grad = np.zeros(len(y), dtype=dtype)
+                grad[split_mask] = r_ + (self.gamma - 1) * r_proj * dr_
+                hess = np.zeros(len(y), dtype=dtype)
+                hess[split_mask] = dr_
+
+            else:
+                r_proj = Q @ (Q.T @ r)
+                grad = r + (self.gamma - 1) * r_proj * dr
+                hess = dr
+
             # We wish to fit one additional tree. Intuitively, one would use
             # is_finished = self.booster.update(fobj=self.objective.objective)
             # for this. This makes a call to self.__inner_predict(0) to get the current
@@ -119,14 +209,11 @@ class AnchorBooster:
                     {"objective": "none"}
                 )._Booster__set_objective_to_none = True
 
-            residuals_masked = residuals[mask]
-            residuals_masked_proj = Q[mask, :] @ (Q[mask, :].T @ residuals_masked)
-            grad[mask] = -residuals_masked - (self.gamma - 1) * residuals_masked_proj
-            grad[~mask] = 0.0
-
             # is_finished is True if there we no splits satisfying the splitting
             # criteria. c.f. https://github.com/microsoft/LightGBM/pull/6890
-            is_finished = self.booster._Booster__boost(grad, mask.astype(dtype))
+            # The hessian is used only for the `min_hessian_in_leaf` parameter to
+            # avoid numerical instabilities.
+            is_finished = self.booster._Booster__boost(grad, hess)
 
             if is_finished:
                 print(f"Finished training after {idx} iterations.")
@@ -137,47 +224,88 @@ class AnchorBooster:
             ).flatten()
             num_leaves = np.max(leaves) + 1
 
+            if self.honest_splits:
+                r_ = r[leaf_mask]
+                dr_ = dr[leaf_mask]
+                Q_ = Q[leaf_mask, :]
+                # Q_ @ (Q_.T @ r_) is Z[mask, :] @ (Z.T @ Z) @ (Z[mask, :]^T @ r_)
+                # That is, we use the full Z matrix to compute its covariance. However,
+                # different number of samples are used, so we need to rescale.
+                r_proj = Q_ @ (Q_.T @ r_)
+                r_proj /= (1 - self.honest_splits_ratio) * self.subsample
+                grad = r_ + (self.gamma - 1) * r_proj * dr_
+                leaves_ = leaves[leaf_mask]
+                ddr_ = ddr[leaf_mask]
+                # if self.gamma > 1 and idx == 9:
+                #     breakpoint()
+            else:
+                leaves_ = leaves
+                dr_ = dr
+                ddr_ = ddr
+                Q_ = Q
+
+            # We wish to do 2nd order updates in the leaves. Since the anchor regression
+            # objective is quadratic, for regression a 2nd order update is equal to the
+            # global minimizer.
             # Let M be the one-hot encoding of the tree's leaf assignments. That is,
-            # M[i, j] = 1 if leaves[i] == j else 0. We wish to select the leaf values
-            # beta to minimize the anchor loss:
-            # loss = || y - M beta ||^2 + (gamma - 1) || P_Z (y - M beta) ||^2
-            #      = || (Id + (gamma - 1) P_Z) (y - M beta) ||^2
-            # The optimal beta is given by
-            # beta = (M^T (Id + (gamma - 1) P_Z) M)^{-1} M^T (Id + (gamma - 1) P_Z) y
-            # with M^T M = diag(np.bincount(leaves)).
-            leaves_masked = leaves[~mask]
-            M = scipy.sparse.csr_matrix(
+            # M[i, j] = 1 if leaves[i] == j else 0.
+            # We have
+            # r = d/df loss(f, y)
+            # dr = d^2/df^2 loss(f, y)
+            # ddr = d^3/df^3 loss(f, y)
+            # The anchor loss is
+            # L = loss(f, y) + (gamma - 1) / 2 * || P_Z r ||^2
+            # d/df L = d/df loss(f, y) + (gamma - 1) P_Z r * dr
+            # d^2/df^2 L = diag(d^2/df^2 loss(f, y)) + (gamma - 1) diag(P_Z r * ddr)
+            #            + (gamma - 1) diag(dr) P_Z diag(dr)
+            #
+            # We do the 2nd order update
+            # beta = - (M^T [d^2/df^2 L] M)^{-1} M^T [d/df L]
+
+            # M^T x = bincount(leaves_masked, weights=x)
+            g = np.bincount(leaves_, weights=grad, minlength=num_leaves)
+
+            # M^T diag(x) M = diag(np.bincount(leaves, weights=x))
+            counts = np.bincount(
+                leaves_,
+                weights=dr_ + (self.gamma - 1) * r_proj * ddr_,
+                minlength=num_leaves,
+            )
+            counts += self.params.get("lambda_l2", 0)
+
+            # If honest_splits is True, it can occur that some leaves have no "leaf"
+            # samples. We make the hessian invertible.
+            counts[counts == 0] = 1
+
+            # Mdr^T P_Z Mdr = (Mdr^T Q) @ (Mdr^T Q)^T
+            # One could also compute this using bincount, but it appears this
+            # version using a sparse matrix is faster.
+            Mdr = scipy.sparse.csr_matrix(
                 (
-                    np.ones_like(leaves_masked),
-                    (np.arange(len(leaves_masked)), leaves_masked),
+                    dr_,
+                    (np.arange(len(leaves_)), leaves_),
                 ),
-                shape=(len(leaves_masked), num_leaves),
+                shape=(len(leaves_), num_leaves),
                 dtype=dtype,
             )
-            B = M.T.dot(Q[~mask, :])  # M^T @ Q of shape (num_leaves, num_anchors)
-            # A = M^T (Id + (gamma - 1) P_Z) M, where M^T M = diag(np.bincount(leaves))
-            counts = np.bincount(leaves_masked, minlength=num_leaves)
-            # There might be some leaves without estimation samples. Set a 1 on the
-            # diagonal to ensure pos. def. of A. The resulting leaf value will be 0.
-            counts[counts == 0] = 1
-            A = np.diag(counts) + (self.gamma - 1) * B @ B.T
-            # b = M^T (Id + (gamma - 1) P_Z) y
-            residuals_masked = residuals[~mask]
-            residuals_masked_proj = Q[~mask, :] @ (Q[~mask, :].T @ residuals_masked)
-            weights = -residuals_masked - (self.gamma - 1) * residuals_masked_proj
-            b = np.bincount(leaves_masked, weights=weights, minlength=num_leaves)
+            B = Mdr.T.dot(Q_)
+            H = (self.gamma - 1) * B @ B.T
+            if self.honest_splits_ratio is not None:
+                H /= (1 - self.honest_splits_ratio) * self.subsample
+            H += np.diag(counts)
 
-            leaf_values = -np.linalg.solve(A, b) * self.params.get("learning_rate", 0.1)
+            # Compute the 2nd order update
+            leaf_values = -np.linalg.solve(H, g) * self.params.get("learning_rate", 0.1)
 
             for ldx, val in enumerate(leaf_values):
                 self.booster.set_leaf_output(idx, ldx, val)
 
-            # Ensure residuals == y - self.init_score_ - self.booster.predict(X)
-            residuals -= leaf_values[leaves]
+            # Ensure f == self.init_score_ + self.booster.predict(X)
+            f += leaf_values[leaves]
 
         return self
 
-    def predict(self, X, num_iteration=-1):
+    def predict(self, X, raw_score=False, **kwargs):
         """
         Predict the outcome.
 
@@ -188,6 +316,8 @@ class AnchorBooster:
         num_iteration : int
             Number of boosting iterations to use. If -1, all are used. Else, needs to be
             in [0, num_boost_round].
+        raw_score : bool
+            If True, returns scores. If False, returns predicted probabilities.
         """
         if self.booster is None:
             raise ValueError("AnchorBoost has not yet been fitted.")
@@ -195,5 +325,11 @@ class AnchorBooster:
         if _POLARS_INSTALLED and isinstance(X, pl.DataFrame):
             X = X.to_arrow()
 
-        scores = self.booster.predict(X, num_iteration=num_iteration, raw_score=True)
-        return scores + self.init_score_
+        scores = self.booster.predict(X, raw_score=True, **kwargs)
+
+        if self.objective == "logistic" and not raw_score:
+            return scipy.special.expit(scores + self.init_score_)
+        elif self.objective == "probit" and not raw_score:
+            return scipy.stats.norm.cdf(scores + self.init_score_)
+        else:
+            return scores + self.init_score_
--- tests/test_anchor_boosting.py
+++ tests/test_anchor_boosting.py
@@ -1,47 +1,102 @@
+from functools import partial
+
+import lightgbm as lgb
 import numpy as np
 import pytest
+import scipy
 
 from anchorboosting import AnchorBooster
 from anchorboosting.simulate import f1, simulate
 
 
-@pytest.mark.parametrize("gamma", [1.0, 2.0])
-def test_anchor_boosting(gamma):
+@pytest.mark.parametrize("honest_splits_ratio", [None, 0.3])
+@pytest.mark.parametrize("gamma", [1.0, 2.0, 100])
+@pytest.mark.parametrize("objective", ["regression", "logistic", "probit"])
+def test_anchor_boosting_second_order(gamma, objective, honest_splits_ratio):
     learning_rate = 0.1
-    num_leaves = 3
-    n = 1000
-
-    rng = np.random.default_rng(0)
-    mask = np.ones(n, dtype=bool)
-    mask[500:] = False
-    for _ in range(10):
-        rng.shuffle(mask)
+    num_leaves = 5
+    n = 200
+    num_boost_round = 10
 
     x, y, a = simulate(f1, n=n, shift=0, seed=0)
 
-    model = AnchorBooster(gamma=gamma, num_boost_round=10, num_leaves=num_leaves)
+    if objective in ["logistic", "probit"]:
+        y = (y > 0).astype(int)
+
+    model = AnchorBooster(
+        gamma=gamma,
+        num_boost_round=num_boost_round,
+        num_leaves=num_leaves,
+        objective=objective,
+        learning_rate=learning_rate,
+        honest_splits=honest_splits_ratio is not None,
+        honest_splits_ratio=honest_splits_ratio,
+    )
     model.fit(x, y, Z=a)
 
-    x, y = x[~mask, :], y[~mask]
-    residuals = y - model.predict(x, num_iteration=9)
+    mask = np.ones(n, dtype=bool)
+    if honest_splits_ratio is not None:
+        rng = np.random.default_rng(0)
+        mask[: int(n * honest_splits_ratio)] = False
+        for i in range(num_boost_round):
+            perm = rng.permutation(n)
+            mask = mask[perm]
+
+    f = model.predict(x, num_iteration=num_boost_round - 1, raw_score=True)
 
     leaves = model.booster.predict(
-        x, pred_leaf=True, start_iteration=9, num_iteration=1
+        x, pred_leaf=True, start_iteration=num_boost_round - 1, num_iteration=1
     ).flatten()
 
-    # In Anchor Regression, we optimize || (Id + (gamma - 1) P_Z) (y - M beta) ||^2
-    # In honest splits, we want to use y[mask] only to estimate split parameters, not
-    # the leaf values. However, we still use all of the anchor `a` to estimate its
-    # covariance `a.T @ a`.
-    M = np.equal.outer(leaves, np.arange(num_leaves)).astype(float)
-    MTM = M.T @ M + (gamma - 1) * M.T @ a[~mask, :] @ np.linalg.solve(
-        a.T @ a, a[~mask, :].T @ M
-    )
-    MTy = M.T @ residuals + (gamma - 1) * M.T @ a[~mask, :] @ np.linalg.solve(
-        a.T @ a, a[~mask, :].T @ residuals
-    )
+    def regression_loss(leaf_values, mask):
+        residuals = y[mask] - f[mask] - leaf_values[leaves[mask]]
+        Pa_residuals = a[mask, :] @ np.linalg.solve(a.T @ a, a[mask, :].T @ residuals)
+        Pa_residuals /= mask.mean()
+        return np.sum(np.square(residuals)) + (gamma - 1) * residuals.T @ Pa_residuals
 
-    expected_leaf_values = np.linalg.solve(MTM, MTy) * learning_rate
+    def classification_loss(leaf_values, mask):
+        scores = f[mask] + leaf_values[leaves[mask]]
+        p = 1 / (1 + np.exp(-scores))
+        residuals = y[mask] - p
+        Pa_residuals = a[mask, :] @ np.linalg.solve(a.T @ a, a[mask, :].T @ residuals)
+        Pa_residuals /= mask.mean()
+        return (
+            np.sum(-np.log(np.where(y[mask] == 1, p, 1 - p)))
+            + (gamma - 1) / 2 * residuals.T @ Pa_residuals
+        )
+
+    def probit_loss(leaf_values, mask):
+        scores = f[mask] + leaf_values[leaves[mask]]
+        p = scipy.stats.norm.cdf(scores)
+        dp = scipy.stats.norm.pdf(scores)
+        losses = -np.log(np.where(y[mask] == 1, p, 1 - p))
+        dl = np.where(y[mask] == 1, -dp / p, dp / (1 - p))
+
+        Pa_dl = a[mask, :] @ np.linalg.solve(a.T @ a, a[mask, :].T @ dl)
+        Pa_dl /= mask.mean()
+        return np.sum(losses) + (gamma - 1) / 2 * dl.T @ Pa_dl
+
+    if objective == "regression":
+        loss = partial(regression_loss, mask=mask)
+    elif objective == "probit":
+        loss = partial(probit_loss, mask=mask)
+    else:
+        loss = partial(classification_loss, mask=mask)
+
+    def vectorize(f):
+        def f_(x):
+            shape = x.shape
+            x = x.reshape((shape[0], -1))
+            out = np.array([f(x[:, i]) for i in range(x.shape[1])])
+            out = out.reshape(shape[1:])
+            return out
+
+        return f_
+
+    grad = scipy.optimize.approx_fprime(np.zeros(num_leaves), loss, 1e-6)
+    hess = scipy.differentiate.hessian(vectorize(loss), np.zeros(num_leaves))
+    expected_leaf_values = -np.linalg.solve(hess.ddf, grad) * learning_rate
+
     for i in range(num_leaves):
         assert np.allclose(
             model.booster.get_leaf_output(9, i),
@@ -49,3 +104,121 @@ def test_anchor_boosting(gamma):
             atol=1e-5,
             rtol=1e-5,
         )
+
+
+@pytest.mark.parametrize("gamma", [1, 10])
+@pytest.mark.parametrize("objective", ["logistic", "regression", "probit"])
+def test_anchor_boosting_decreases_loss(gamma, objective):
+    num_leaves = 5
+    n = 1000
+
+    x, y, a = simulate(f1, n=n, shift=0, seed=0)
+    if objective in ["logistic", "probit"]:
+        y = (y > 0).astype(int)
+
+    model = AnchorBooster(
+        gamma=gamma,
+        num_boost_round=10,
+        num_leaves=num_leaves,
+        objective=objective,
+        honest_splits=False,
+    )
+    model.fit(x, y, Z=a)
+
+    def regression_loss(y, f, a):
+        residuals = y - f
+        Pa_residuals = a @ np.linalg.solve(a.T @ a, a.T @ residuals)
+        return np.sum(np.square(residuals) + (gamma - 1) * np.square(Pa_residuals))
+
+    def classification_loss(y, f, a):
+        p = 1 / (1 + np.exp(-f))
+        residuals = y - p
+        Pa_residuals = a @ np.linalg.solve(a.T @ a, a.T @ residuals)
+        return np.mean(
+            -np.log(np.where(y == 1, p, 1 - p)) + (gamma - 1) * np.square(Pa_residuals)
+        )
+
+    def probit_loss(y, f, a):
+        p = scipy.stats.norm.cdf(f)
+        dp = scipy.stats.norm.pdf(f)
+        losses = -np.log(np.where(y == 1, p, 1 - p))
+        dl = np.where(y == 1, -dp / p, dp / (1 - p))
+
+        Pa_dl = a @ np.linalg.solve(a.T @ a, a.T @ dl)
+        return np.sum(losses) + (gamma - 1) * dl.T @ Pa_dl
+
+    if objective == "regression":
+        loss = regression_loss
+    elif objective == "probit":
+        loss = probit_loss
+    else:
+        loss = classification_loss
+
+    loss_value = np.inf
+    for idx in range(10):
+        f = model.predict(x, num_iteration=idx + 1, raw_score=True)
+        new_loss_value = loss(y, f, a)
+
+        if idx > 0:
+            assert new_loss_value < loss_value
+
+        loss_value = new_loss_value
+
+
+@pytest.mark.parametrize("objective", ["regression", "logistic"])
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        {},
+        {"max_depth": -1},
+        {"num_leaves": 63},
+        {"min_split_gain": 0.002},
+        {"lambda_l2": 0.1},
+    ],
+)
+def test_compare_anchor_boosting_to_lgbm(objective, parameters):
+    X, y, a = simulate(f1, shift=0, seed=0)
+
+    if objective == "logistic":
+        y = (y > 0).astype(int)
+
+    lgbm_model = lgb.train(
+        params={
+            "learning_rate": 0.1,
+            "objective": "binary" if objective == "logistic" else objective,
+            **parameters,
+        },
+        train_set=lgb.Dataset(X, y),
+        num_boost_round=10,
+    )
+
+    anchor_booster = AnchorBooster(
+        gamma=1,
+        num_boost_round=10,
+        objective=objective,
+        learning_rate=0.1,
+        **parameters,
+    ).fit(X, y, Z=a)
+
+    lgbm_pred = lgbm_model.predict(X)
+    anchor_booster_pred = anchor_booster.predict(X)
+
+    np.testing.assert_allclose(lgbm_pred, anchor_booster_pred, rtol=1e-5)
+
+
+@pytest.mark.parametrize("objective", ["regression", "logistic", "probit"])
+def test_anchor_booster_init_score(objective):
+    X, y, a = simulate(f1, shift=0, seed=0)
+
+    if objective in ["logistic", "probit"]:
+        y = (y > 0).astype(int)
+
+    anchor_booster = AnchorBooster(
+        gamma=1,
+        num_boost_round=0,
+        objective=objective,
+        learning_rate=0.1,
+    ).fit(X, y, Z=a)
+
+    predictions = anchor_booster.predict(X)
+    assert np.allclose(predictions, np.ones_like(y) * y.mean())
